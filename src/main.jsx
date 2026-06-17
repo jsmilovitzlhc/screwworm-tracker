f397e698ec7aa2aeeb9968af1b28e348aa5879da
Jacob Smilovitz <jake@longholdco.com>
2026-06-17 13:48:58 -0400

    Restore /widget route for WidgetLayout embed

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './layouts/layouts.css'
 import DenseLayout from './layouts/DenseLayout.jsx'
+import WidgetLayout from './layouts/WidgetLayout.jsx'
+
+function Router() {
+  if (window.location.pathname === '/widget') return <WidgetLayout />;
+  return <DenseLayout />;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DenseLayout />
+    <Router />
   </StrictMode>,
 )
