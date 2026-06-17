6ff504a683fadcec58684f952a993015b02e73c9
Jacob Smilovitz <jake@longholdco.com>
2026-06-17 12:56:07 -0400

    Make DenseLayout the only view, remove unused layouts from routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './layouts/layouts.css'
-import App from './App.jsx'
 import DenseLayout from './layouts/DenseLayout.jsx'
-import NewsLayout from './layouts/NewsLayout.jsx'
-import CompactLayout from './layouts/CompactLayout.jsx'
-import WidgetLayout from './layouts/WidgetLayout.jsx'
-
-function Router() {
-  const path = window.location.pathname;
-  if (path === '/v/dense') return <DenseLayout />;
-  if (path === '/v/news') return <NewsLayout />;
-  if (path === '/v/compact') return <CompactLayout />;
-  if (path === '/widget') return <WidgetLayout />;
-  return <App />;
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router />
+    <DenseLayout />
   </StrictMode>,
 )
